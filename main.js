5bf1e5fc589381e24ca92a5dbad3f3d5b0a8a090
W. PATINO <ppwj@yahoo.com>
2022-05-17 01:24:37 -0400

    Rediseño total

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
 const app = document.getElementById('typewriter');
 
+const uc = document.getElementById('underc');
+
 const typewriter = new Typewriter(app, {
     loop: true,
     delay:90
 });
 
 typewriter
+  .typeString('Atención a domicilio!')
+  .pauseFor(2800)
+  .start();
+
+  underc
   .typeString('Atención a domicilio!')
   .pauseFor(2800)
   .start();
